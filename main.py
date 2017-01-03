1b06b5bf6ddaffa34d7cfc2b238abc8d7446f152
HSJMDMG <lynj.y@qq.com>
2017-01-04 00:37:48 +0800

    finish all the pages except the CRF

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ def home():
 def guide():
     return render_template('guide.html')
 
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
+
+
 @app.route('/presentation')
 @app.route('/presentation/<hwname>')
 def presentation(hwname=None):
@@ -63,6 +68,30 @@ def upload_file3():
 
 
     return render_template('hw3.html', resultData = result , alertbar = 1)
+
+
+
+@app.route('/hw4text', methods=['POST'])
+def post_text4():
+    if (len(request.form['content']) < 3):
+        return render_template('hw4.html', inputData = request.form['content'], alertbar = 2)
+
+    result = hw3Chinese(request.form['content'].encode('gbk'));
+    return render_template('hw4.html', inputData = request.form['content'], resultData = result , alertbar = 1)
+
+@app.route('/hw4file', methods=['GET', 'POST'])
+def upload_file4():
+    if request.method == 'POST':
+        f = request.files['filecontent']
+        f.save('static/uploads/data.txt')
+        inputData = open("static/uploads/data.txt").read()
+
+        result = hw3Chinese(inputData);
+
+
+    return render_template('hw4.html', resultData = result , alertbar = 1)
+
+
 
 
 
